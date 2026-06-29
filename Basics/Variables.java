7ef309af9b37abc5153a66137d57d2283f693efb
Pari Singh <272533974+Parisinghh@users.noreply.github.com>
2026-06-30 02:43:57 +0530

    Add variable programs in java
    Basic programs showing variable declaration, initialization, and usage in Java.

---
+++ Basics/Variables.java
@@ -0,0 +1,16 @@
+  public class Variables {
+    public static void main(String[] args) {
+        
+        // declaring variables
+        int age = 18;
+        double height = 5.6;
+        char grade = 'A';
+        String name = "Pari";
+
+        // printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+    }
+}
