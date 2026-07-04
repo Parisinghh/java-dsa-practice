b5115f403de511a3d7e58cc778aa0027a46b8529
Pari Singh <272533974+Parisinghh@users.noreply.github.com>
2026-07-04 23:41:32 +0530

    Implement array length finder in Java
    This program prompts the user to enter the size of an array, takes input for the array elements, and prints the length of the array.

---
+++ 03-Arrays/FindArrayLength.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array Length: " + arr.length);
+
+        sc.close();
+    }
+}
