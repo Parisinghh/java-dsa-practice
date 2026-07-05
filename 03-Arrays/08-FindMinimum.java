1421f9a6a3e9cd990f01fc6eddbeeb85481a464f
Pari Singh <272533974+Parisinghh@users.noreply.github.com>
2026-07-05 23:33:25 +0530

    Add MinimumArray class to find minimum value
    Implement a program to find the minimum element in an array.

---
+++ 03-Arrays/08-FindMinimum.java
@@ -0,0 +1,18 @@
+public class MinimumArray {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 7, 89, 34, 56};
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Minimum element: " + min);
+    }
+}
+
+//Minimum element: 7
