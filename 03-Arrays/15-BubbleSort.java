526af9a7976242093ecff67a6d5dca346e09a1b3
Pari Singh <272533974+Parisinghh@users.noreply.github.com>
2026-07-07 16:16:12 +0530

    Implement Bubble Sort algorithm in Java
    - Added BubbleSort.java implementation.
    - Used nested loops to sort the array in ascending order.
    - Optimized the algorithm using a swapped flag for early termination.
    - Printed the sorted array after sorting.

---
+++ 03-Arrays/15-BubbleSort.java
@@ -0,0 +1,32 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
